5b50443ce9df388d63aa264c1af1a23245786d36
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-03-24 12:33:46 +0100

    Update test: Use `tape` instead of `mocha`

--- test/index.js
+++ test/index.js
@@ -1,64 +1,62 @@
-var assert = require('assert')
+var test = require('tape')
 var Trace = require('..')
 
-suite('URL Shorteners', function () {
-  this.timeout(10000)
-
-  suite.skip('Twitter (t.co)', function () {
-    test('should receive 3xx redirect for "https://t.co/fW61qZgOWm"', function (done) {
+test('URL Shorteners', function (t) {
+  t.skip('Twitter (t.co)', function (t) {
+    t.test('should receive 3xx redirect for "https://t.co/fW61qZgOWm"', function () {
       new Trace('https://t.co/fW61qZgOWm')
         .resume().once('end', function () {
-          assert.ok(this.statusCode < 400 && this.statusCode >= 300, 'HTTP ' + this.statusCode)
-          done()
+          t.ok(this.statusCode < 400 && this.statusCode >= 300, 'HTTP ' + this.statusCode)
+          t.end()
         })
     })
 
-    test('should receive 3xx redirect for "t.co/fW61qZgOWm"', function (done) {
+    t.test('should receive 3xx redirect for "t.co/fW61qZgOWm"', function () {
       new Trace('t.co/fW61qZgOWm')
         .resume().once('end', function () {
-          assert.ok(this.statusCode < 400 && this.statusCode >= 300, 'HTTP ' + this.statusCode)
-          done()
+          t.ok(this.statusCode < 400 && this.statusCode >= 300, 'HTTP ' + this.statusCode)
+          t.end()
         })
     })
   })
 
-  suite('New York Times (nyti.ms)', function () {
-    test('should see tracking cookies from "nyti.ms/1QETHgV"', function (done) {
+  t.test('New York Times (nyti.ms)', function (t) {
+    t.test('should see tracking cookies from "nyti.ms/1QETHgV"', function () {
       new Trace('nyti.ms/1QETHgV')
         .resume().once('end', function () {
-          assert.equal(this.statusCode, 200)
-          done()
+          t.equal(this.statusCode, 200)
+          t.end()
         })
     })
   })
 
-  suite('Bit.ly', function () {
-    test('should see 1 more hop after "http://bit.ly/berlin-nodejs-meetup"', function (done) {
+  t.test('Bit.ly', function (t) {
+    t.test('should see 1 more hop after "http://bit.ly/berlin-nodejs-meetup"', function () {
       var hops = 0
       new Trace('bit.ly/berlin-nodejs-meetup')
         .on('data', function () { hops++ })
         .once('end', function () {
-          assert.equal(this.statusCode, 200)
-          assert.ok(hops > 1)
-          done()
+          t.equal(this.statusCode, 200)
+          t.ok(hops > 1)
+          t.end()
         })
     })
   })
 })
 
-suite('Workarounds', function () {
-  suite('User-Agent', function () {
-    test('Facebook should not redirect to "facebook.com/unsupportedbrowser"', function (done) {
+test('Workarounds', function (t) {
+  t.test('User-Agent', function (t) {
+    t.test('Facebook should not redirect to "facebook.com/unsupportedbrowser"', function () {
       var lastHop = null
       new Trace('facebook.com')
         .on('data', function (hop) {
-          assert.ok(/facebook.com\/unsupportedbrowser/.test(hop) === false)
+          t.ok(/facebook.com\/unsupportedbrowser/.test(hop) === false)
           lastHop = hop
         })
         .once('end', function () {
-          assert.equal(this.statusCode, 200)
-          assert.equal('https://www.facebook.com', lastHop.url)
-          done()
+          t.equal(this.statusCode, 200)
+          t.equal('https://www.facebook.com', lastHop.url)
+          t.end()
         })
     })
   })
