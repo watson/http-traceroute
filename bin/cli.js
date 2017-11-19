99d818f06007d3228c1c8aceb25469675e449e9a
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-11-19 12:27:23 +0100

    Add help & version cli options

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,21 @@
 #!/usr/bin/env node
 'use strict'
 
+var argv = process.argv.slice(2)
+
+if (argv.includes('--version') || argv.includes('-v')) {
+  console.log(require('../package').version)
+  process.exit(0)
+}
+
+if (argv.includes('--help') || argv.includes('-h')) {
+  var usage = `
+  http-traceroute [url]\n
+`
+  process.stdout.write(usage)
+  process.exit(0)
+}
+
 var Trace = require('..')
 var chalk = require('chalk')
 
@@ -8,7 +23,7 @@ var trace = new Trace(process.argv[2])
   .once('error', function (error) {
     var url = error.url ? chalk.gray('- ' + error.url) : ''
     console.log(chalk.red('ERROR'), error.message, url)
-    process.exit(0)
+    process.exit(1)
   })
   .once('end', function () {
     console.log('Trace finished in %s using %s', chalk.cyan(trace.time + ' ms'), chalk.cyan(trace.hops + ' hop' + (trace.hops > 1 ? 's' : '')))
